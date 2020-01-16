8a5561600cc694b9bd811576e1d76ff7a449923e
LordotU <LevshinO@gmail.com>
2020-01-16 22:05:44 +0000

    chore: Update deps and minify build

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,45 @@
-import resolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
+
+const isProd = process.env.NODE_ENV === 'production'
+const isBrowser = !! process.env.CMILD_BROWSER
+
+const plugins = [
+  resolve({
+    browser: true,
+    jsnext: true,
+  }),
+  replace({
+    'process.browser': isBrowser,
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  }),
+  babel({
+    babelrc: false,
+    exclude: 'node_modules/**',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: isBrowser ? { browsers: 'last 2 versions' } : { node: true },
+          modules: false,
+        },
+      ],
+    ],
+  }),
+]
+
+if (isProd) {
+  plugins.push(terser())
+}
 
 export default {
   input: 'src/index.js',
   output: {
-    file: `build/${!! process.env.CMILD_BROWSER ? 'browser' : 'node'}.js`,
+    file: `build/${isBrowser ? 'browser' : 'node'}.js`,
     format: 'cjs',
   },
-  plugins: [
-    resolve({
-      browser: true,
-      jsnext: true,
-    }),
-    replace({
-      'process.browser': !! process.env.CMILD_BROWSER,
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-    }),
-    babel({
-      babelrc: false,
-      exclude: 'node_modules/**',
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: !! process.env.CMILD_BROWSER ? { browsers: 'last 2 versions' } : { node: true },
-            modules: false,
-          },
-        ],
-      ],
-    }),
-  ],
+  plugins,
 }
